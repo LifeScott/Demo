bdb4410609580dbb0acde98bad7a79fa3155836e
LifeScott <scottthushyanthan@outlook.it>
2019-02-27 16:31:46 +0100

    Alert and other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Demo';
+  appTitle = 'PerDindiRindina';
+  toolTip = 'come volete voi';
+  inputVar: string;
+
+  changeTitle(){
+    this.appTitle='ho cambiato il titolo di nuovo';
+    this.inputVar='sempre quello che volete voi parte 2';
+  }
+  showAlert(){
+    alert(this.inputVar);
+  }
 }
