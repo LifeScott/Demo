989e2ca3fda514fa7abe2a2d5a39a3acd19f682d
LifeScott <scottthushyanthan@outlook.it>
2019-03-04 11:05:28 +0100

    prova interpolazione

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
+  vara = 1;
+  varb = 3;
+
+  getVal(): any {
+    return this.vara + this.varb;
+  }
 }
