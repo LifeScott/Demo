96a0a037fac2ab9d2d7d505a683785cd40a28ddc
LifeScott <scottthushyanthan@outlook.it>
2019-03-04 14:11:04 +0100

    ngclass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,26 @@ export class AppComponent {
   vara = 1;
   varb = 3;
   link = 'https://www.google.it';
+  oggetto = { value: 'valoreOggetto' };
 
+
+
+  metodo(): boolean {
+    return 
+  }
+
+  get definizioneClassi() {
+    return {
+      classea: this.vara === 1,
+      classeb: this.varb === 1,
+      classec: this.vara === this.varb
+    }
+  };
+
+  mostraVariabile(value) {
+    alert(value);
+    this.oggetto = undefined;
+  }
 
   getSomma(): any {
     return this.vara + this.varb;
